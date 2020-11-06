83c45e13e8a622b7bc7b993416388d8a4682f934
anishmoktan <anish.tmngo@gmail.com>
2020-11-06 12:03:27 -0500

    added

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 from alphavantage import search_stock
+import requests
 
-search_stock('AAPfL')
+data = requests.get('https://ipinfo.io/json')
+data = data.json()
+print(data['city'])
+print(data['region'])
+
+
+search_stock('AAPL')
+search_stock('AMZN')
